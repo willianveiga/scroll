83217d1a95dd79eeb09e6449de79043daa02d47f
Willian Gustavo Veiga <wiltave@gmail.com>
2013-01-28 21:07:29 -0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 Scroll
 ======
+
+Back 0.1
+--------
+
+Show/hide a link that scrolls back to top according to the scroll position.
+
+Example
+-------
+
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8" />
+            <title>Example</title>
+            <style>
+            /**
+             * DON'T FORGET TO ADD THE ELEMENT'S POSITION!
+             * CONFIGURE THE WAY YOU WANT ...
+             */
+            #scroll-up-link {
+                bottom: 0;
+                right: 0;
+            }
+            </style>
+        </head>
+        <body>
+            <!-- A lot of content here ... -->
+            <a id="scroll-up-link" href="#">Scroll Up!</a>
+            <script src="scroll.back.min.js"></script>
+            <script>
+            /*globals window, document, WillianVeiga */
+            window.addEventListener('load', function () {
+                'use strict';
+    
+                var link = document.getElementsByTagName('a')[0],
+                    scrollBack = new WillianVeiga.Scroll.Back(link);
+    
+                scrollBack.bindEvents();
+            });
+            </script>
+        </body>
+    </html>
+
+You can unbind all events calling unbindEvents:
+
+    scrollBack.unbindEvents();
+
+Please, report bugs or fork it :)
---
+++ back.js
@@ -0,0 +1,59 @@
+/**
+    Copyright 2013 Willian Gustavo Veiga
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*globals window */
+var WillianVeiga = WillianVeiga || {};
+WillianVeiga.Scroll = WillianVeiga.Scroll || {};
+
+(function (namespace) {
+    'use strict';
+
+    var element,
+        clickEvent,
+        scrollEvent,
+        setInitialElementStyle;
+
+    namespace.Back = function (inputElement) {
+        element = inputElement;
+        setInitialElementStyle();
+    };
+
+    namespace.Back.prototype.bindEvents = function () {
+        element.addEventListener('click', clickEvent);
+        window.addEventListener('scroll', scrollEvent);
+    };
+
+    namespace.Back.prototype.unbindEvents = function () {
+        element.removeEventListener('click', clickEvent);
+        window.removeEventListener('scroll', scrollEvent);
+    };
+
+    setInitialElementStyle = function () {
+        var style = element.style;
+        style.position = 'fixed';
+        style.display = 'none';
+    };
+
+    clickEvent = function (event) {
+        event.preventDefault();
+        window.scrollTo(0, 0);
+    };
+
+    scrollEvent = function () {
+        element.style.display = (this.scrollY > 10 ? 'block' : 'none');
+    };
+}(WillianVeiga.Scroll));
